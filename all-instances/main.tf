fa5d41e9071593171a357137a3d653e3fcae3dac
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-11 14:43:49 +0530

    updating ./learning/learning-terraform

---
+++ all-instances/main.tf
@@ -0,0 +1,15 @@
+module "sg" {
+  source = "security-group"
+}
+
+module "instance" {
+  source = "ec2-instances"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-storages"
+    key    = "ec2-instances/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
