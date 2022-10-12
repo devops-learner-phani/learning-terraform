c7188abdb45524207af2b8984a0b262968b31f99
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-12 08:26:39 +0530

    updating ./learning/learning-terraform

--- 04-loops/approach-1/main.tf
+++ 04-loops/approach-1/main.tf
@@ -4,10 +4,19 @@ default = 2
 
 resource "aws_instance" "web" {
   count         = var.no-of-instances
-  ami           = "ami-0eccf89e870877654"
+  ami           = "ami-0eccf89e87087765"
   instance_type = "t3.micro"
 
   tags = {
     Name = "terraform-${count.index + 1}"
   }
 }
+
+output "public_ip_address" {
+  value = aws_instance.web.*.public_ip
+}
+
+
+
+
+
---
+++ 05-conditions/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0eccf89e87087765"
+  instance_type = var.instance_type == null ? "t2.micro" : var.instance_type
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+variable "instance_type" {
+  default = null
+}
+
+
+
+
