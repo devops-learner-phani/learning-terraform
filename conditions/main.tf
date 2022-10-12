a3bf185b8957fed197054ce4200f6d178462585a
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-12 10:31:23 +0530

    updating ./learning/learning-terraform

---
+++ conditions/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0eccf89e87087765"
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+variable "instance_type" {
+  default = null
+}
